a71dc62bfac0ccf65ab3a785e8f5a4dfc7727a92
Brett Nicholas <brett.r.nicholas.15@dartmouth.edu>
2023-05-10 13:33:20 -0600

    added copyright

--- moducryptolib_wolfssl.c
+++ moducryptolib_wolfssl.c
@@ -5,6 +5,7 @@
  *
  * Copyright (c) 2017-2018 Paul Sokolovsky
  * Copyright (c) 2018 Yonatan Goldschmidt
+ * Copyright (c) 2023 wolfSSL Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
